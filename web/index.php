7718ee060cdecdbd17927ba153070a53718d64b4
Ruslan Ismailov <chifek@gmail.com>
2017-01-20 23:51:11 +0600

    Modify index.php

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/../views',
 ));
 $blogPosts = array(
     1 => array(
