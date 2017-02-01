752fd4d440e4e29c72e667e2d88d342a3f2eaebb
Ruslan Ismailov <ruslan@mavix.io>
2017-02-01 15:55:34 +0600

    Renew prject

--- web/index.php
+++ web/index.php
@@ -1,58 +1,31 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
-
 $app = new Silex\Application();
 $app['debug'] = true;
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
-));
+
 $blogPosts = array(
     1 => array(
-        'date'      => '2011-03-29',
-        'author'    => 'Ruslan',
-        'title'     => 'Using Silex',
-        'body'      => '...',
-    ),
-    2 => array(
-        'date' => '2017-01-19',
-        'author' => 'Samat',
-        'title' => 'Silex Test',
-        'body' => 'It is working!',
+        'date' => '2011-03-29',
+        'author' => 'igorw',
+        'title' => 'Using Silex',
+        'body' => '...',
     ),
 );
-
 $app->get('/blog', function () use ($blogPosts) {
     $output = '';
     foreach ($blogPosts as $post) {
         $output .= $post['title'];
         $output .= '<br />';
-        $output .= $post['date'];
-        $output .= '<br />';
     }
-
     return $output;
 });
-
 $app->get('/blog/{id}', function (Silex\Application $app, $id) use ($blogPosts) {
     if (!isset($blogPosts[$id])) {
         $app->abort(404, "Post $id does not exist.");
     }
-
     $post = $blogPosts[$id];
-
-    return  "<h1>Title: {$post['title']}</h1>".
-        "<b>Author: {$post['author']}</b>".
-        "<p>Text: {$post['body']}</p>";
-});
-
-$app->post('/feedback', function (Request $request) {
-    $message = $request->get('message');
-    mail('feedback@yoursite.com', '[YourSite] Feedback', $message);
-
-    return new Response('Thank you for your feedback!', 201);
+    return "<h1>{$post['title']}</h1>".
+        "<p>{$post['body']}</p>";
 });
 $app->run();
